3649bd318764c33438fcb583882497fe87b6a849
Subhajyoti Dash <dashjitu3@gmail.com>
2019-04-19 20:14:54 +0530

    Initial commit

---
+++ regloginapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from.models import Reg
+class RegForm(forms.ModelForm):
+    class Meta:
+        model = Reg
+        widgets = {'pwd':forms.PasswordInput(),}
+        fields = ['user','pwd','fname','lname','dob','mob']
+class LoginForm(forms.Form):
+    user = forms.CharField(max_length=20)
+    pwd = forms.CharField(widget=forms.PasswordInput())
+
+
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\subha\PycharmProjects\subharegloginaproj\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
